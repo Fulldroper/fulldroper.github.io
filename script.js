e21543b4d09abc0b4a9e3d3aae34b55d21c6280c
full_droper <full_droper@pm.me>
2022-07-08 23:49:18 +0300

    add git pins

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 window.onload = () => {
-    const changeTheme = bool => (document.body || document.querySelector("body")).classList[bool ? "add" : "remove"]("darkmode")
-    const theme = document.querySelector("#dark-switch")
-    changeTheme(theme.checked)
-    document.querySelector("#dark-switch").onchange = e => {
-        changeTheme(e.target.checked);
+  const changeTheme = bool => (document.body || document.querySelector("body")).classList[bool ? "add" : "remove"]("darkmode");
+  const theme = document.querySelector("#dark-switch");
+  const loadPins = async arr => {
+    const pinsLoader = document.querySelector("#pinsLoader")
+    for (const value of arr) {
+      const svg = await axios.get(`https://fd-kofuku.herokuapp.com/?q=https://github-readme-stats.vercel.app/api/pin/?username=fulldroper%amprepo=${value}`)
+      pinsLoader.innerHTML += `<a href="https://github.com/Fulldroper/${value}">${
+        svg.data
+      }</a>`
     }
+  };
+  changeTheme(theme.checked);
+  loadPins([
+    'cors-proxy',
+    'tocken-session',
+    'file-server',
+    'Sierpinski-triangle',
+    'caesarCipher',
+    'color-convertor'
+  ]);
+  document.querySelector("#dark-switch").onchange = e => {
+    changeTheme(e.target.checked);
+  }
 }
